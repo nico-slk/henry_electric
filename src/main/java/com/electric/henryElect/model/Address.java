6ec1458f274b6e0602e7b0ea69b9be1e58830d63
nico-slk <nico1991.6@gmail.com>
2021-04-19 18:25:19 -0300

    class

--- src/main/java/com/electric/henryElect/model/Address.java
+++ src/main/java/com/electric/henryElect/model/Address.java
@@ -1,4 +1,17 @@
 package com.electric.henryElect.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Address {
+
+    private String street;
+    private String number;
+    private LightMeter lightMeter;
+
 }
--- src/main/java/com/electric/henryElect/model/Invoice.java
+++ src/main/java/com/electric/henryElect/model/Invoice.java
@@ -10,22 +10,15 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-public abstract class Invoice implements Serializable {
+public class Invoice{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private LightMeter lightMeter; // Numero de medidor
-    private String client; // Cliente
-    private String address; //Domicilio
-    private Integer initialMedition; //Medición inicial
-    private Integer finalMedition; //Medición final
-    private Integer totalConsumption; //Consumo total en Kwh
     private LocalDate timeInitialMedition; //Fecha y hora medición inicial
     private LocalDate timeFinalMedition; //Fecha y hora medición final
     private Double rate; //Tipo de tarifa
-    private Double total = totalConsumption * rate; //Total a pagar (Consumo * Tarifa)
+    private Double total; //Total a pagar (Consumo * Tarifa)
     private Boolean pay;
 
 }
